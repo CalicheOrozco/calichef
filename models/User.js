f7642758f55b7307872ea620dc64b0bd7315d194
CalicheOrozco <caliche2da1@gmail.com>
2025-04-18 03:13:43 -0700

    feat(models): add favoriteCollections field to User schema

--- models/User.js
+++ models/User.js
@@ -27,6 +27,10 @@ const UserSchema = new mongoose.Schema({
     type: [String],
     default: []
   },
+  favoriteCollections: {
+    type: [String],
+    default: []
+  },
   createdAt: {
     type: Date,
     default: Date.now
